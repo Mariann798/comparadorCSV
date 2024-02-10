a00d1c4ba497772ba0c50a268284911c1b1d7a31
Mariano García Yalungo <garcia.mariano92@gmail.com>
2024-02-10 16:46:01 -0300

    fist commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import argparse
+import pandas
+parser = argparse.ArgumentParser(
+    prog="comprare",
+    description="Compare CSVs",
+    epilog="by Mariann798"
+)
+
+parser.add_argument("-t", "--this", help="", required=True)
+parser.add_argument("-w", "--with_that", help="", required=True)
+parser.add_argument("-c", "--column", help="", required=True)
+
+parser = parser.parse_args()
+
+def read_csv(path:str) -> pandas.DataFrame:
+    return pandas.read_csv(path, sep=";")
+
+def compare_files(path1:str, path2:str, column):
+    df1 = read_csv(path1)
+    print("Len File 1: ", len(df1))
+    df2 = read_csv(path2)
+    print("Len File 2: ", len(df2))
+    
+    data_in_df1_not_in_df2 = set(df1[column]) - set(df2[column])
+    return list(data_in_df1_not_in_df2)
+
+if __name__ == '__main__':
+    res = compare_files(parser.this, parser.with_that, parser.column)
+    
+    print("Resultados: ", len(res))
+    print("Emails: ", res)
